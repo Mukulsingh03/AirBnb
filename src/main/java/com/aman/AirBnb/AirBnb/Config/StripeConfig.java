8e1380969bbe74c6eff198304dbefff0445185ce
Mukul Rawat <mukulrawat2101@gmail.com>
2026-02-05 21:49:50 +0530

    Initial commit - Airbnb backend

---
+++ src/main/java/com/aman/AirBnb/AirBnb/Config/StripeConfig.java
@@ -0,0 +1,20 @@
+package com.aman.AirBnb.AirBnb.Config;
+
+import com.stripe.Stripe;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("prod")
+public class StripeConfig {
+
+    @Value("${stripe.secret.key}")
+    private String secretKey;
+
+    @PostConstruct
+    public void init() {
+        Stripe.apiKey = secretKey;
+    }
+}
